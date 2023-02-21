7fb624ef72c73c44b410b4f8eabb4a8fbed4402c
ManoDimitri <gabriel.dimitri@outlook.com>
2023-02-21 19:27:19 -0300

    cpf validator

--- main.py
+++ main.py
@@ -55,6 +55,18 @@ async def calcular(interaction, valor: str):
 @tree.command(name="geradorcpf", description="Vai gerar um CPF", guild=discord.Object(id=874833976085344307))
 async def geradorcpf(interaction):
     cpf = CPF.generate()
-    await interaction.response.send_message(f"{cpf}")
+    formatedcpf = CPF.format(cpf)
+    await interaction.response.send_message(f"{formatedcpf}")
+
+@tree.command(name="validadorcpf", description="Valida o CPF informado, informa o cpf sem os . e -", guild=discord.Object(id=874833976085344307))
+async def validaorcpf(interaction, cpf: int):
+    fcpf = CPF.format(cpf)
+    validadorcpf = CPF.validate(fcpf)
+    if validadorcpf == True:
+        await interaction.response.send_message(f"O CPF: {fcpf} é verdadeiro!")
+    elif validadorcpf == False:
+        await interaction.response.send_message(f"O CPF: {fcpf} é falso!")
+    
+
 
 client.run(discord_token)
