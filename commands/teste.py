eb01ddb7634cfddcc6f974b69c9ec169d9218cd9
ManoDimitri <gabriel.dimitri@outlook.com>
2023-04-27 11:49:31 -0300

    test

--- commands/teste.py
+++ commands/teste.py
@@ -1,7 +1,7 @@
 import discord
 from slash import *
+from main import guilds
 
-@tree.command(name = "teste", description = "Testando o comando")
-async def teste(interaction):
-
+@tree.command(name = "teste", description = "Testando o comando", guild=discord.Object(id=guilds))
+async def teste(interaction):   
     await interaction.response.send_message("Visão, eu to online")
